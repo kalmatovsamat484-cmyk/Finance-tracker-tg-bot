0795f5c44d589bdc36e442103a989f8657ee1477
Samat <kalmatovsamat484@gmail.com>
2026-09-09 20:22:57 +0600

    new finance  bot +income +balance +start

---
+++ src/main/java/org/example/FinanceBot.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class FinanceBot extends TelegramLongPollingBot {
+
+        FinanceService financeService = new FinanceService();
+
+    @Override
+    public String getBotUsername() {
+        return "demo12_finance_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "8862497801:AAEkm62f6ZYSICpKLixdQdcKrmxbvVeCrLo";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            System.out.println(messageText);
+
+            if (messageText.equals("/start")) {
+
+                SendMessage message = new SendMessage();
+
+                message.setChatId(String.valueOf(chatId));
+                message.setText("Welcome to Finance Tracker!");
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else if (messageText.equals("/balance")) {
+
+                SendMessage message = new SendMessage();
+
+                message.setChatId(String.valueOf(chatId));
+                message.setText(
+                        "Your balance: " + financeService.getBalance()
+                );
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (messageText.startsWith("/income")){
+                String[] parts= messageText.split(" ");
+                if (parts.length==2){
+
+                    try{
+                        SendMessage message=new SendMessage();
+                        double amount = Double.parseDouble(parts[1]);
+                        financeService.addIncome(amount);
+                        message.setChatId(String.valueOf(chatId));
+                        if (amount<=0){
+                            message.setText("Income must be greater than zero");
+                            execute(message);
+                        }
+                        else {
+                            financeService.addIncome(amount);
+                            message.setText("Income: +" + amount);
+                            execute(message);
+                        }
+                    }catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    catch (NumberFormatException e){
+                        SendMessage message = new SendMessage();
+                        message.setChatId(String.valueOf(chatId));
+                        message.setText("Amount must be a number");
+
+                        try {
+                            execute(message);
+                        } catch (TelegramApiException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    }
+                }
+                else {
+                   try {
+                       SendMessage message = new SendMessage();
+                       message.setChatId(String.valueOf(chatId));
+                       message.setText("Please enter an amount. Example: /income 5000");
+                       execute(message);
+                   }catch (TelegramApiException e) {
+                       e.printStackTrace();
+                   }
+                }
+
+            }
+        }
+    }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
+
     public static void main(String[] args) {
-ConsoleMenu consoleMenu = new ConsoleMenu();
-consoleMenu.consoleMenu();
+
+        try {
+            TelegramBotsApi botsApi =
+                    new TelegramBotsApi(DefaultBotSession.class);
+
+            botsApi.registerBot(new FinanceBot());
+
+            System.out.println("Bot started!");
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
